c97c14c5a83de894a8af16a1f4c437968ec66f4c
judith pizzolato <judith pizzolato>
2016-08-07 23:42:29 -0400

    updated sql

--- Homework/bamazon.sql
+++ Homework/bamazon.sql
@@ -10,42 +10,17 @@ CREATE TABLE products(
     StockQuantity INT(10) NOT NULL,
     primary key(ItemID)
 );
+
+select * from products;
 -- Creates new rows containing data in all named columns --
 INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("Blue Dress","Womens","10.00",100);
-
-INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("Red Dress","Womens","20.00",100);
-
-INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("Yellow Dress","Womens","15.00",100);
-
-INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("Purple Dress","Womens","10.00",100);
-
-INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("Green Dress","Womens","20.00",100);
-
-INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("Blue Shirt","Mens","10.00",100);
-
-INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("Red Shirt","Mens","20.00",100);
-
-INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("Yellow Shirt","Mens","15.00",100);
-
-INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("Purple Shirt","Mens","10.00",100);
-
-INSERT INTO products (ProductName,DepartmentName,Price,StockQuantity)
-VALUES ("Green Shirt","Mens","20.00",100);
-
-
-
--- Updates the row where the column name is Blue Dress --
-UPDATE products
-SET ProductName="Blue Dress", DepartmentName="Womens", Price=15.00
-WHERE ItemID=1;
-
-SELECT * FROM products;
+VALUES  ("Blue Dress","Womens",10.00,100);
+        ("Red Dress","Womens",20.00,100);
+        ("Yellow Dress","Womens",15.00,100);
+        ("Purple Dress","Womens",10.00,100);
+        ("Green Dress","Womens",20.00,100);
+        ("Blue Shirt","Mens",10.00,100);
+        ("Red Shirt","Mens",20.00,100);
+        ("Yellow Shirt","Mens",15.00,100);
+        ("Purple Shirt","Mens",10.00,100);
+        ("Green Shirt","Mens",20.00,100);
